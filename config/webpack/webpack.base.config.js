9f27f4674b3f5a4245a042471fee5bc8117b884a
Valerii <valera-22-10@mail.ru>
2019-10-04 00:31:22 +0700

    init

---
+++ config/webpack/webpack.base.config.js
@@ -0,0 +1,129 @@
+const path = require('path')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+
+const PATH = {
+    entry: path.join( __dirname, '../config.js'),
+    dist: path.join( __dirname, '../../public/'),
+    postcssConfig: path.join(__dirname, '../postcss.config.js'),
+    src: path.join(__dirname, '../../src'),
+}
+
+module.exports = {
+
+    externals: {
+        path: PATH
+    },
+
+    entry:{
+        build: PATH.entry
+    },
+
+    output: {
+        filename: 'js/[name].[hash].js',
+        path: PATH.dist,
+        publicPath: '/'
+    },
+
+    optimization:{
+        splitChunks:{
+            cacheGroups:{
+                vendor:{
+                    name:'vendors',
+                    test: /node_modules/,
+                    chunks:'all',
+                    enforce: true
+                }
+            }
+        }
+    },
+
+    module:{
+        rules:[
+            {
+                test:/\.js$/,
+                loader: 'babel-loader',
+                exclude:'/node_modules/'
+            },
+            {
+                test:/\.css$/,
+                use: [
+                    'style-loader',
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: 'css-loader',
+                        options: { 
+                            sourceMap: true
+                        }
+                    }, {
+                        loader: 'postcss-loader',
+                        options: { 
+                            sourceMap: true, 
+                            config: { 
+                                path: PATH.postcssConfig
+                            }
+                        }
+                    }
+                ]
+            },
+            {
+                test:/\.scss$/,
+                use: [
+                    'style-loader',
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: 'css-loader',
+                        options: { 
+                            sourceMap: true
+                        }
+                    }, {
+                        loader: 'postcss-loader',
+                        options: { 
+                            sourceMap: true, 
+                            config: { 
+                                path: PATH.postcssConfig
+                            }
+                        }
+                    }, {
+                        loader: 'sass-loader',
+                        options: { 
+                            sourceMap: true 
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.(png|jpg|gif|svg)$/,
+                loader: 'file-loader',
+                options: {
+                    name: '[name].[ext]',
+                }
+            }
+        ]
+    },
+    plugins: [
+
+        new MiniCssExtractPlugin({
+            filename: `css/[name].[hash].css`
+        }),
+
+        new HtmlWebpackPlugin({
+            template: `${PATH.src}/index.html`,
+            filename: './index.html',
+            //inject:false // отключает автоматическую вставку
+        }),
+
+        new CopyWebpackPlugin([
+            {
+                from: `${PATH.src}/images`, 
+                to: `images`
+            },
+            {
+                from: `${PATH.src}/fonts`, 
+                to: `css/fonts`
+            },
+        ])
+
+    ]
+}
---
+++ config/webpack/webpack.dev.conf.js
@@ -0,0 +1,31 @@
+const merge = require('webpack-merge')
+const baseWebpackConfig = require('./webpack.base.config')
+const webpack = require('webpack')
+
+console.log(baseWebpackConfig.externals.path)
+const devWebpackConfig  = merge(baseWebpackConfig, {
+
+    mode: 'development',
+  
+    devServer:{
+        contentBase: baseWebpackConfig.externals.path.dist,
+        overlay: {
+            warnings: true,
+            errors: true
+        },
+        port: 3000,
+    },
+    
+    devtool: 'cheap-module-eval-source-map',
+    plugins: [
+        new webpack.SourceMapDevToolPlugin({
+          filename: "[file].map"
+        }),
+    ]
+
+});
+  
+
+module.exports = new Promise((resolve, reject) => {
+    resolve(devWebpackConfig )
+})
